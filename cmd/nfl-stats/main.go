04ab0be949b796e54d37bb9a54f272d399799e7e
Tom Szymanski <szymanski.tom@gmail.com>
2021-11-13 10:15:01 -0600

    dynamic URL

--- cmd/nfl-stats/main.go
+++ cmd/nfl-stats/main.go
@@ -1,14 +1,17 @@
 package nflstats
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 // GetScores fetches current NFL game scores
 func GetScores() error {
-	// TODO: make dynamic
-	URL := "https://api.nfl.com/experience/v1/games?season=2021&seasonType=REG&week=10"
-	_, err := http.Get(URL)
-	// TODO:
-	// example response
-	// {"ss":[["Sun","13:00:00","Final",,"MIA","26","BUF","56",,,"58408",,"REG17","2020"],["Sun","13:00:00","Final",,"BAL","38","CIN","3",,,"58411",,"REG17","2020"],["Sun","13:00:00","Final",,"PIT","22","CLE","24",,,"58412",,"REG17","2020"],["Sun","13:00:00","Final",,"MIN","37","DET","35",,,"58413",,"REG17","2020"],["Sun","13:00:00","Final",,"NYJ","14","NE","28",,,"58415",,"REG17","2020"],["Sun","13:00:00","Final",,"DAL","19","NYG","23",,,"58416",,"REG17","2020"],["Sun","13:00:00","Final",,"ATL","27","TB","44",,,"58418",,"REG17","2020"],["Sun","16:25:00","Final",,"NO","33","CAR","7",,,"58409",,"REG17","2020"],["Sun","16:25:00","Final",,"GB","35","CHI","16",,,"58410",,"REG17","2020"],["Sun","16:25:00","Final",,"LV","32","DEN","31",,,"58419",,"REG17","2020"],["Sun","16:25:00","Final",,"TEN","41","HOU","38",,,"58420",,"REG17","2020"],["Sun","16:25:00","Final",,"JAX","14","IND","28",,,"58421",,"REG17","2020"],["Sun","16:25:00","Final",,"LAC","38","KC","21",,,"58414",,"REG17","2020"],["Sun","16:25:00","Final",,"ARI","7","LA","18",,,"58422",,"REG17","2020"],["Sun","16:25:00","Final",,"SEA","26","SF","23",,,"58423",,"REG17","2020"],["Sun","20:20:00","Final",,"WAS","20","PHI","14",,,"58417",,"REG17","2020"]]}
+	_, err := http.Get(nflURL(2021, 11))
 	return err
+}
+
+func nflURL(season, week int) string {
+	base := "https://api.nfl.com/experience/v1/games"
+	return fmt.Sprintf("%s?season=%d&seasonType=REG&week=%d", base, season, week)
 }
