970f7b4330224cfeef4593e92c03855e4c00f8a2
Tom Szymanski <szymanski.tom@gmail.com>
2021-11-16 07:15:06 -0600

    get ESPN json

--- cmd/nfl-stats/main.go
+++ cmd/nfl-stats/main.go
@@ -1,20 +1,16 @@
 package nflstats
 
 import (
-	"fmt"
-	"log"
 	"net/http"
 )
 
 // GetScores fetches current NFL game scores
 func GetScores() error {
-	_, err := http.Get(nflURL(2021, 11, "REG"))
+	_, err := http.Get(espnNFLScoresURL())
 	return err
 }
 
-func nflURL(season, week int, seasonType string) string {
-	base := "https://api.nfl.com/experience/v1/games"
-	url := fmt.Sprintf("%s?season=%d&seasonType=%s&week=%d", base, season, seasonType, week)
-	log.Printf("nflURL: %+v", url)
-	return url
+// TODO: switch to ESPN:
+func espnNFLScoresURL() string {
+	return "http://site.api.espn.com/apis/site/v2/sports/football/nfl/scoreboard"
 }
