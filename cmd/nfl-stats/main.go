442a5c95a863f0568cc9538fa1db5b5799db0273
Tom Szymanski <szymanski.tom@gmail.com>
2021-11-18 05:59:03 -0600

    catch that error

--- cmd/nfl-stats/main.go
+++ cmd/nfl-stats/main.go
@@ -1,12 +1,18 @@
 package nflstats
 
 import (
+	"fmt"
 	"net/http"
 )
 
 // GetScores fetches current NFL game scores
 func GetScores() error {
-	_, err := http.Get(espnNFLScoresURL())
+	URL := espnNFLScoresURL()
+	resp, err := http.Get(URL)
+	if err != nil {
+		return fmt.Errorf("mlbStats#GetSchedule: Get %s, error: %s", URL, err)
+	}
+	defer resp.Body.Close()
 	return err
 }
 
