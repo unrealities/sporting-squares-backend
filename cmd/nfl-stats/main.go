d9035c7f8cf2c0336617001c3e94653a3f99c2a7
Tom Szymanski <szymanski.tom@gmail.com>
2021-11-22 09:41:09 -0500

    ESPN your face

--- cmd/nfl-stats/main.go
+++ cmd/nfl-stats/main.go
@@ -7,32 +7,26 @@ import (
 	"net/http"
 )
 
-// GetScores fetches current NFL game scores
-func GetScores() (Scores, error) {
-	URL := espnNFLScoresURL()
+// GetESPNScores fetches current NFL game scores
+func GetESPNScores() (EspnNFLScores, error) {
+	URL := "http://site.api.espn.com/apis/site/v2/sports/football/nfl/scoreboard"
 	resp, err := http.Get(URL)
 	if err != nil {
-		return Scores{}, fmt.Errorf("nflStats#GetScores: Get %s, error: %s", URL, err)
+		return EspnNFLScores{}, fmt.Errorf("nflStats#GetScores: Get %s, error: %s", URL, err)
 	}
 
 	defer resp.Body.Close()
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return Scores{}, fmt.Errorf("nflStats#GetScores: reading Get response body error: %s", err)
+		return EspnNFLScores{}, fmt.Errorf("nflStats#GetScores: reading Get response body error: %s", err)
 	}
 
 	espnNFLScoresResp := EspnNFLScores{}
 	err = json.Unmarshal(body, &espnNFLScoresResp)
 	if err != nil {
-		return Scores{}, fmt.Errorf("nflStats#GetScores: unmarshal error: %s", err)
+		return EspnNFLScores{}, fmt.Errorf("nflStats#GetScores: unmarshal error: %s", err)
 	}
 
-	// TODO: remove the unnecessary fields from the ESPN response
-	return Scores{}, nil
-}
-
-// TODO: switch to ESPN:
-func espnNFLScoresURL() string {
-	return "http://site.api.espn.com/apis/site/v2/sports/football/nfl/scoreboard"
+	return EspnNFLScores{}, nil
 }
