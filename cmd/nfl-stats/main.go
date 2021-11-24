0779db184f08d618da4420a7c9ff096c6a3316dc
Tom Szymanski <szymanski.tom@gmail.com>
2021-11-24 10:07:50 -0500

    Get those scores

--- cmd/nfl-stats/main.go
+++ cmd/nfl-stats/main.go
@@ -9,6 +9,10 @@ import (
 
 // TODO: reduce GetESPNScores struct to app readable data
 func GetScores() (Scores, error) {
+	_, err := GetESPNScores()
+	if err != nil {
+		return Scores{}, fmt.Errorf("nflStats@GetScores: GetESPNScores, error: %s", err)
+	}
 	return Scores{}, nil
 }
 
@@ -17,20 +21,20 @@ func GetESPNScores() (EspnNFLScores, error) {
 	URL := "http://site.api.espn.com/apis/site/v2/sports/football/nfl/scoreboard"
 	resp, err := http.Get(URL)
 	if err != nil {
-		return EspnNFLScores{}, fmt.Errorf("nflStats#GetScores: Get %s, error: %s", URL, err)
+		return EspnNFLScores{}, fmt.Errorf("nflStats#GetESPNScores: Get %s, error: %s", URL, err)
 	}
 
 	defer resp.Body.Close()
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return EspnNFLScores{}, fmt.Errorf("nflStats#GetScores: reading Get response body error: %s", err)
+		return EspnNFLScores{}, fmt.Errorf("nflStats#GetESPNScores: reading Get response body error: %s", err)
 	}
 
 	espnNFLScoresResp := EspnNFLScores{}
 	err = json.Unmarshal(body, &espnNFLScoresResp)
 	if err != nil {
-		return EspnNFLScores{}, fmt.Errorf("nflStats#GetScores: unmarshal error: %s", err)
+		return EspnNFLScores{}, fmt.Errorf("nflStats#GetESPNScores: unmarshal error: %s", err)
 	}
 
 	return EspnNFLScores{}, nil
