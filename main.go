2867773caddeb26f2b5777dfa6367d7ee1ee1be6
Tom Szymanski <szymanski.tom@gmail.com>
2021-12-19 08:28:11 -0500

    add id to output

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	games, _ := nflStats.GetGames()
 	for i, g := range games {
-		fmt.Printf("Game %v: %s (%v) @ %s (%v) [%v][%v] {%v [%v]}\n",
-			i, g.AwayTeam, g.AwayScore, g.HomeTeam, g.HomeScore, g.Quarter, g.Time, g.Odds.Details, g.Odds.OverUnder)
+		fmt.Printf("Game %v[%v]: %s (%v) @ %s (%v) [%v][%v] {%v [%v]}\n",
+			i, g.ID, g.AwayTeam, g.AwayScore, g.HomeTeam, g.HomeScore, g.Quarter, g.Time, g.Odds.Details, g.Odds.OverUnder)
 	}
 }
